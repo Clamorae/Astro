a132d2f927cd1811612f8cc08f7b3556d03b7df2
Clamorae <trombiniquentin@gmail.com>
2023-10-30 09:13:53 +0800

    added first quizz

---
+++ S07/question.py
@@ -0,0 +1,13 @@
+import requests
+
+
+response = requests.get("https://s3b.astro.ncu.edu.tw/ai_202309/data/q07.data")
+
+splitted = response.content.split(b"\n")
+print(splitted)
+counted = []
+
+for word in splitted:
+    if word not in counted:
+        print(f"the word {word} appear in the table {splitted.count(word)} times")
+        counted.append(word)
