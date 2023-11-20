52bfaa9ace304d70f58fb111f9c2863bcb0b38a6
Clamorae <trombiniquentin@gmail.com>
2023-11-20 11:41:26 +0800

    exercises for week 10 and 9

---
+++ S09/S09_02.py
@@ -0,0 +1,60 @@
+import numpy as np
+import astropy.units as units
+import astropy.coordinates as coord
+import matplotlib.figure as fig
+import matplotlib.backends.backend_agg as back
+
+u_ha = units.hourangle
+u_deg = units.deg
+
+data_ra = np.array([])
+data_dec = np.array([])
+data_l = np.array([])
+data_b = np.array([])
+
+with open("./Astro/S09/dataQ2.txt","r") as f:
+    lines = f.readlines()
+
+for line in lines:
+    if "#" not in line:
+        #print(line)
+        splitted = line.split()
+        try:
+            ra_rad = float(splitted[8])
+        except:
+            continue
+        dec_rad = float(splitted[10])
+
+        #coordinate = coord.SkyCoord(ha,deg,unit=(u_ha,u_deg))
+
+        # ra_rad = coordinate.ra.radian
+        # dec_rad = coordinate.dec.radian
+
+        #l_rad = coordinate.galactic.l.radian
+        #b_rad = coordinate.galactic.b.radian
+
+        if ra_rad > np.pi:
+            ra_rad -= 2.0 * np.pi
+        # if l_rad > np.pi:
+        #     l_rad -= 2.0 * np.pi
+
+        data_ra = np.append(data_ra, ra_rad)
+        data_dec = np.append(data_dec, dec_rad)
+        # data_l = np.append(data_l, l_rad)
+        # data_b = np.append(data_b, b_rad)
+    
+gal_lon = np.linspace(0.001 , 359.999 , 1000) * u_deg
+gal_lat = np.zeros(1000) * u_deg
+gal_coord = coord.Galactic(l = gal_lon, b = gal_lat)
+gal_ra = gal_coord.transform_to(coord.ICRS).ra.wrap_at(180.0 * u_deg).radian
+gal_dec = gal_coord.transform_to(coord.ICRS).dec.radian
+
+fig = fig.Figure()
+canvas = back.FigureCanvasAgg(fig)
+ax = fig.add_subplot(111, projection = "aitoff")
+
+ax.grid()
+
+ax.scatter(data_ra, data_dec, marker = "o", c = "blue", alpha =0.3)
+
+fig.savefig("Astro/S09/E2.png")
---
+++ S10/S10_01.py
@@ -0,0 +1,42 @@
+import numpy as np
+import scipy as sp
+import matplotlib.figure as figure
+import matplotlib.backends.backend_agg as back
+
+with open("./Astro/S10/EA.txt","r") as f:
+    lines = f.readlines()
+
+lines = lines[111:]
+dist = []
+vel = []
+
+for line in lines:
+    splitted = line.split()
+    try:
+        curr_dist = float(splitted[-2])
+        curr_vel = float(splitted[-3])
+    except:
+        continue
+
+    dist = np.append(dist,curr_dist)
+    vel = np.append(vel,curr_vel)
+
+def func (x , H0 ):
+    y = H0 * x
+    return (y)
+
+H0 = 100
+
+popt , pcov = sp.optimize.curve_fit(func, dist, vel, p0 = [H0])
+H0_bestfit = popt[0]
+fitted_x = np.linspace(0.0, 25.0, 50)
+fitted_y = func(fitted_x, H0_bestfit)
+
+fig = figure.Figure()
+canvas = back.FigureCanvasAgg(fig)
+ax = fig.add_subplot()
+
+ax.plot(dist,vel, linestyle = "None", markersize = 2, marker= "o")
+ax.plot(fitted_x, fitted_y, linestyle = "--", linewidth = 3, color = "red", zorder = 0.1) 
+
+fig.savefig("./Astro/S10/EA.png")
